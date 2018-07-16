fab26372b2116593237fe0ec424041ce87c0530b
pckn\tadamasa <sqwer712@yahoo.co.jp>
2018-07-17 03:47:30 +0900

    クラスの課題

---
+++ CampChallenge_java/013.クラス/クラスの課題/Class2.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tadamasa
+ */
+public class Class2 extends Class1 {
+
+//初期値の設定なし
+   public void clearProfile () {
+       
+        this.name = "";
+        this.age = 0; 
+        this.adress = ""; 
+   }
+
+   
+    
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CampChallenge_java/013.クラス/クラスの課題/Main2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tadamasa
+ */
+public class Main2 {
+    
+    public static void main(String[] args){
+        
+       Class2 pannda = new Class2(); 
+            
+       pannda.setProfile("ヤンくん",10,"北京"); 
+       
+       
+       pannda.printProfile();
+       
+       pannda.clearProfile();
+       
+       pannda.printProfile();
+    
+    }
+ 
+}
+
+
+
